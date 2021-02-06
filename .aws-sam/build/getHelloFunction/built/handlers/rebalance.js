ad5b4c00d5a061969610ec720cd901fa3dd7c88c
Rahul Sethuram <rahul@sethuram.com>
2021-02-06 14:44:05 +0100

    Refactor

--- .aws-sam/build/getHelloFunction/built/handlers/rebalance.js
+++ .aws-sam/build/getHelloFunction/built/handlers/rebalance.js
@@ -1,11 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.rebalanceHandler = void 0;
+const maticjs_1 = require("@maticnetwork/maticjs");
+const vector_utils_1 = require("@connext/vector-utils");
+const ethers_1 = require("ethers");
+const ETH_CHAIN_ID = 1;
+const MATIC_CHAIN_ID = 137;
 const rebalanceHandler = async () => {
-    // add rebalancing code
+    const chainProviders = JSON.parse(process.env.CHAIN_PROVIDERS);
+    const rebalanceConfig = JSON.parse(process.env.REBALANCE_CONFIG);
+    const pk = JSON.parse(process.env.ROUTER_KEY);
+    const routerSigner = new vector_utils_1.ChannelSigner(pk);
+    console.log("routerSigner.publicIdentifier: ", routerSigner.publicIdentifier);
+    console.log("routerSigner.address: ", routerSigner.address);
+    console.log("chainProviders: ", chainProviders);
+    const parentProvider = chainProviders[ETH_CHAIN_ID];
+    const maticProvider = chainProviders[MATIC_CHAIN_ID];
+    const parentEthProvider = new ethers_1.providers.JsonRpcProvider(parentProvider);
+    const maticEthProvider = new ethers_1.providers.JsonRpcProvider(parentProvider);
+    const parentWallet = new ethers_1.Wallet(pk).connect(parentEthProvider);
+    const maticWallet = new ethers_1.Wallet(pk).connect(maticEthProvider);
+    const maticPOSClient = new maticjs_1.MaticPOSClient({
+        network: "mainnet",
+        version: "v1",
+        parentProvider,
+        maticProvider,
+    });
+    // check router signer address balances against ratios
+    for (const rebalance of rebalanceConfig) {
+        // const parentBalance = await parentEthProvider;
+    }
+    // rebalance between matic/mainnet as necessary
+    // https://docs.matic.network/docs/develop/ethereum-matic/pos/using-sdk/erc20
     return {
         statusCode: 200,
-        body: `Hello World, this is Connext faucet`,
+        body: JSON.stringify({ matic: maticPOSClient.network }),
         headers: {
             "Access-Control-Allow-Origin": "*",
         },
--- src-ts/handlers/rebalance.ts
+++ src-ts/handlers/rebalance.ts
@@ -1,12 +1,60 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { MaticPOSClient } from "@maticnetwork/maticjs";
+import { ChannelSigner } from "@connext/vector-utils";
+import {
+  BigNumber,
+  constants,
+  Contract,
+  providers,
+  utils,
+  Wallet,
+} from "ethers";
+import { ERC20Abi } from "@connext/vector-types";
+
+type RebalanceConfig = {
+  maticAssetId: string;
+  maticAssetIdDecimals: string;
+  ethAssetId: string;
+  ethAssetIdDecimals: string;
+  targetRatio: number;
+  allowedDeviationPct: number;
+}[];
+
+const ETH_CHAIN_ID = 1;
+const MATIC_CHAIN_ID = 137;
+
+const getOnchainBalanceForAsset = async (
+  assetId: string,
+  balanceOf: string,
+  provider: providers.JsonRpcProvider
+): Promise<BigNumber> => {
+  if (assetId === constants.AddressZero) {
+    return provider.getBalance(balanceOf);
+  } else {
+    return new Contract(assetId, ERC20Abi, provider).balanceOf(balanceOf);
+  }
+};
 
 export const rebalanceHandler = async (): Promise<APIGatewayProxyResult> => {
-  const providers = JSON.parse(process.env.CHAIN_PROVIDERS);
-  console.log("providers: ", providers);
+  const chainProviders = JSON.parse(process.env.CHAIN_PROVIDERS);
+  const rebalanceConfig: RebalanceConfig = JSON.parse(
+    process.env.REBALANCE_CONFIG
+  );
 
-  const parentProvider = providers["1"];
-  const maticProvider = providers["137"];
+  const pk = JSON.parse(process.env.ROUTER_KEY);
+  const routerSigner = new ChannelSigner(pk);
+  console.log("routerSigner.publicIdentifier: ", routerSigner.publicIdentifier);
+  console.log("routerSigner.address: ", routerSigner.address);
+  console.log("chainProviders: ", chainProviders);
+
+  const parentProvider = chainProviders[ETH_CHAIN_ID];
+  const maticProvider = chainProviders[MATIC_CHAIN_ID];
+
+  const parentEthProvider = new providers.JsonRpcProvider(parentProvider);
+  const maticEthProvider = new providers.JsonRpcProvider(parentProvider);
+
+  const parentWallet = new Wallet(pk).connect(parentEthProvider);
+  const maticWallet = new Wallet(pk).connect(maticEthProvider);
 
   const maticPOSClient = new MaticPOSClient({
     network: "mainnet",
@@ -16,6 +64,33 @@ export const rebalanceHandler = async (): Promise<APIGatewayProxyResult> => {
   });
 
   // check router signer address balances against ratios
+  for (const rebalance of rebalanceConfig) {
+    const parentBalance = await getOnchainBalanceForAsset(
+      rebalance.ethAssetId,
+      routerSigner.address,
+      parentEthProvider
+    );
+
+    const maticBalance = await getOnchainBalanceForAsset(
+      rebalance.maticAssetId,
+      routerSigner.address,
+      maticEthProvider
+    );
+
+    let ratio: BigNumber;
+    if (parentBalance.gt(maticBalance)) {
+      ratio = parentBalance.div(maticBalance);
+    } else {
+      ratio = maticBalance.div(parentBalance);
+    }
+    console.log("ratio: ", ratio);
+
+    const difference = ratio.sub(utils.parseEther("1"));
+    console.log("difference: ", difference);
+
+    const discrepancy = parseFloat(utils.formatEther(difference));
+    console.log("discrepancy: ", discrepancy);
+  }
 
   // rebalance between matic/mainnet as necessary
 
@@ -23,7 +98,7 @@ export const rebalanceHandler = async (): Promise<APIGatewayProxyResult> => {
 
   return {
     statusCode: 200,
-    body: `Hello World, this is Connext faucet`,
+    body: JSON.stringify({ matic: maticPOSClient.network }),
     headers: {
       "Access-Control-Allow-Origin": "*",
     },
