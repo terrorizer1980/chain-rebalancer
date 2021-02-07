b89bb196e9bd6e6259786536b2473e8c65abed24
Rahul Sethuram <rahul@sethuram.com>
2021-02-07 15:40:15 +0100

    Rebalance code

--- src-ts/handlers/rebalance.ts
+++ src-ts/handlers/rebalance.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { MaticPOSClient } from "@maticnetwork/maticjs";
-import { ChannelSigner } from "@connext/vector-utils";
+import { calculateExchangeAmount, ChannelSigner } from "@connext/vector-utils";
 import {
   BigNumber,
   constants,
@@ -23,7 +23,15 @@ type RebalanceConfig = {
 const ETH_CHAIN_ID = 1;
 const MATIC_CHAIN_ID = 137;
 
-const getOnchainBalanceForAsset = async (
+const chainProviders = JSON.parse(process.env.CHAIN_PROVIDERS);
+const rebalanceConfig: RebalanceConfig = JSON.parse(
+  process.env.REBALANCE_CONFIG
+);
+
+const pk = JSON.parse(process.env.ROUTER_KEY);
+const routerSigner = new ChannelSigner(pk);
+
+export const getOnchainBalanceForAsset = async (
   assetId: string,
   balanceOf: string,
   provider: providers.JsonRpcProvider
@@ -35,14 +43,37 @@ const getOnchainBalanceForAsset = async (
   }
 };
 
-export const rebalanceHandler = async (): Promise<APIGatewayProxyResult> => {
-  const chainProviders = JSON.parse(process.env.CHAIN_PROVIDERS);
-  const rebalanceConfig: RebalanceConfig = JSON.parse(
-    process.env.REBALANCE_CONFIG
+export const bridgeToMatic = async (
+  amountToBridge: BigNumber,
+  assetId: string,
+  maticPOSClient: MaticPOSClient
+): Promise<{ txHash: string }> => {
+  await maticPOSClient.approveERC20ForDeposit(assetId, amountToBridge, {
+    from: routerSigner.address,
+  });
+  await maticPOSClient.depositERC20ForUser(
+    assetId,
+    routerSigner.address,
+    amountToBridge,
+    {
+      from: routerSigner.address,
+      gasPrice: "10000000000",
+    }
   );
+  return { txHash: "" };
+};
 
-  const pk = JSON.parse(process.env.ROUTER_KEY);
-  const routerSigner = new ChannelSigner(pk);
+export const bridgeToEth = async (
+  amountToBridge: BigNumber,
+  assetId: string,
+  maticPOSClient: MaticPOSClient
+): Promise<{ txHash: string }> => {
+  return { txHash: "" };
+};
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+export const rebalanceHandler = async (): Promise<APIGatewayProxyResult> => {
   console.log("routerSigner.publicIdentifier: ", routerSigner.publicIdentifier);
   console.log("routerSigner.address: ", routerSigner.address);
   console.log("chainProviders: ", chainProviders);
@@ -50,17 +81,17 @@ export const rebalanceHandler = async (): Promise<APIGatewayProxyResult> => {
   const parentProvider = chainProviders[ETH_CHAIN_ID];
   const maticProvider = chainProviders[MATIC_CHAIN_ID];
 
+  const parentWallet = new HDWalletProvider(pk, parentProvider);
+  const maticWallet = new HDWalletProvider(pk, maticProvider);
+
   const parentEthProvider = new providers.JsonRpcProvider(parentProvider);
   const maticEthProvider = new providers.JsonRpcProvider(parentProvider);
-
-  const parentWallet = new Wallet(pk).connect(parentEthProvider);
-  const maticWallet = new Wallet(pk).connect(maticEthProvider);
 
   const maticPOSClient = new MaticPOSClient({
     network: "mainnet",
     version: "v1",
-    parentProvider,
-    maticProvider,
+    parentWallet,
+    maticWallet,
   });
 
   // check router signer address balances against ratios
@@ -70,26 +101,47 @@ export const rebalanceHandler = async (): Promise<APIGatewayProxyResult> => {
       routerSigner.address,
       parentEthProvider
     );
+    console.log("parentBalance: ", parentBalance.toString());
 
     const maticBalance = await getOnchainBalanceForAsset(
       rebalance.maticAssetId,
       routerSigner.address,
       maticEthProvider
     );
+    console.log("maticBalance: ", maticBalance.toString());
 
-    let ratio: BigNumber;
+    let ratio: number;
     if (parentBalance.gt(maticBalance)) {
-      ratio = parentBalance.div(maticBalance);
+      ratio =
+        parseFloat(utils.formatEther(parentBalance)) /
+        parseFloat(utils.formatEther(maticBalance));
     } else {
-      ratio = maticBalance.div(parentBalance);
+      ratio =
+        parseFloat(utils.formatEther(parentBalance)) /
+        parseFloat(utils.formatEther(maticBalance));
     }
     console.log("ratio: ", ratio);
 
-    const difference = ratio.sub(utils.parseEther("1"));
+    const difference = ratio - 1;
     console.log("difference: ", difference);
+    const discrepancyPct = difference * 100;
 
-    const discrepancy = parseFloat(utils.formatEther(difference));
-    console.log("discrepancy: ", discrepancy);
+    if (discrepancyPct > rebalance.allowedDeviationPct) {
+      const total = parentBalance.add(maticBalance);
+      console.log("total: ", total.toString());
+      const target = total.div(2);
+      console.log("target: ", target.toString());
+      let res;
+      if (parentBalance.gt(maticBalance)) {
+        const amountToBridge = parentBalance.sub(target);
+        console.log("amountToBridge: ", amountToBridge.toString());
+        res = await bridgeToMatic(amountToBridge, rebalance.ethAssetId);
+      } else {
+        const amountToBridge = maticBalance.sub(target);
+        console.log("amountToBridge: ", amountToBridge.toString());
+        res = await bridgeToEth(amountToBridge, rebalance.ethAssetId);
+      }
+    }
   }
 
   // rebalance between matic/mainnet as necessary
